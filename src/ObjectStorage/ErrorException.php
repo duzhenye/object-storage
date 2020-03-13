99132a63583387a9052de255aebcdb17a5460b10
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 10:14:45 +0800

    Create ErrorException.php

---
+++ src/ObjectStorage/ErrorException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Object Storage
+ * https://github.com/duzhenye/object-storage
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye\ObjectStorage;
+
+/**
+ * Exception that will be thrown when a library error occurs.
+ */
+class ErrorException extends \ErrorException
+{
+    
+}
