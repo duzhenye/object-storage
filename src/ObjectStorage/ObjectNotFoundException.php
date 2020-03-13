ee145e54d356e6839d974cf941f6a37c3ac110e2
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 10:16:09 +0800

    Create ObjectNotFoundException.php

---
+++ src/ObjectStorage/ObjectNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Object Storage
+ * https://github.com/duzhenye/object-storage
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace Duzhenye\ObjectStorage;
+
+/**
+ * Exception that will be thrown when trying to use a non existent object.
+ */
+class ObjectNotFoundException extends \Duzhenye\ObjectStorage\ErrorException
+{
+    
+}
